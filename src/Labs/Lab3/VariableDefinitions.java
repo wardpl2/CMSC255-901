370088a52916dd175a6871d47a63b36b54edc17d
wardpl2 <wardpl2@vcu.edu>
2022-02-25 19:13:21 -0500

    Added Lab3

---
+++ src/Labs/Lab3/VariableDefinitions.java
@@ -0,0 +1,50 @@
+package Labs.Lab3;
+
+/************************************
+* Variable Definitions
+*************************************
+* Preston Ward
+* 9/7/2021
+* CMSC255-901
+************************************/
+public class VariableDefinitions {
+	public static void main(String[] args) {
+		/**
+		 * initial declaration of numPeople variable
+		 */
+		byte numPeople = 3;
+		/**
+		 * initial declaration of roomNum variable
+		 */
+		int roomNum = 207;
+		/**
+		 * initial declaration of longNum variable
+		 */
+		long longNum = 990123456789L;
+		/**
+		 * initial declaration of price variable
+		 */
+		double price = 32.32;
+		System.out.println("The numPeople variable has a value of " + numPeople);
+		System.out.println("The roomNum variable has a value of " + roomNum);
+		System.out.println("The longNum variable has a value of " + longNum);
+		System.out.println("The price variable has a value of " + price);
+		/**
+		 * modified value of the numPeople variable
+		 */
+		numPeople = 6;
+		/**
+		 * modified value of the roomNum variable
+		 */
+		roomNum = 385;
+		/**
+		 * modified value of the longNum variable
+		 */
+		longNum = 12345678987654321L;
+		/**
+		 * modified value of the price variable
+		 */
+		price = 25.84;
+		
+	}
+}
