17e9f0685058c066b824d150988295f3e1bfd7a9
wardpl2 <wardpl2@vcu.edu>
2022-02-25 19:14:56 -0500

    Added Project1

---
+++ src/Projects/Project1/NameArt.java
@@ -0,0 +1,26 @@
+package Projects.Project1;
+
+/************************************
+* Name Art
+*************************************
+* Preston Ward
+* 8/31/2021
+* CMSC255-901
+************************************/
+public class NameArt {
+	public static void main(String[] args) {
+		System.out.println("\nPreston Ward");
+		System.out.println("Project1 - Name in Code");
+		System.out.println("This project outputs my name in a unique way on the command line");
+		System.out.println("September 2, 2021");
+		System.out.println("CMSC255-901\n");
+		System.out.println(" ________  ________  _______   ________  _________  ________  ________      ");
+		System.out.println("|\\   __  \\|\\   __  \\|\\  ___ \\ |\\   ____\\|\\___   ___\\\\   __  \\|\\   ___  \\    ");
+		System.out.println("\\ \\  \\|\\  \\ \\  \\|\\  \\ \\   __/|\\ \\  \\___|\\|___ \\  \\_\\ \\  \\|\\  \\ \\  \\\\ \\  \\   ");
+		System.out.println(" \\ \\   ____\\ \\   _  _\\ \\  \\_|/_\\ \\_____  \\   \\ \\  \\ \\ \\  \\\\\\  \\ \\  \\\\ \\  \\  ");
+		System.out.println("  \\ \\  \\___|\\ \\  \\\\  \\\\ \\  \\_|\\ \\|____|\\  \\   \\ \\  \\ \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ ");
+		System.out.println("   \\ \\__\\    \\ \\__\\\\ _\\\\ \\_______\\____\\_\\  \\   \\ \\__\\ \\ \\_______\\ \\__\\\\ \\__\\");
+		System.out.println("    \\|__|     \\|__|\\|__|\\|_______|\\_________\\   \\|__|  \\|_______|\\|__| \\|__|");
+		System.out.println("                                 \\|_________|                               ");
+	}
+}
