89fc81029e9ade7fe7283132247b100627654217
wardpl2 <wardpl2@vcu.edu>
2022-02-25 19:12:42 -0500

    Added Lab1

---
+++ src/Labs/Lab1/Hello.java
@@ -0,0 +1,22 @@
+package Labs.Lab1;
+
+/************************************
+* Hello
+*************************************
+* Preston Ward
+* 8/24/2021
+* CMSC255-901
+************************************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	System.out.println("Preston Ward");
+	System.out.println("Hello.java");
+	System.out.println("Java programming assignment for Lab1");
+	System.out.println("8/24/2021");
+	System.out.println("CMSC255-901");
+	System.out.println();
+	}
+}
